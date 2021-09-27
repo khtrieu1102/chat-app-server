e3c6ced1e68f3d8751f7926877537795c4ae945e
KhacTrieu <khtrieu1102@gmail.com>
2021-09-27 23:28:25 +0700

    Update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {
 	removeUser,
 	getUser,
 	getAllUser,
-	getUsersInRoom
+	getUsersInRoom,
 } = require("./users");
 const PORT = process.env.PORT || 5000;
 
@@ -27,21 +27,29 @@ io.on("connection", (socket) => {
 	socket.on("join", ({ name, room }, callback) => {
 		const { error, user } = addUser({ id: socket.id, name, room });
 
-		if (error) return callback(error);
+		if (error) {
+			// console.log(error);
+			return callback({ error: error });
+		}
 
-		console.log(`welcome, ${name}`);
-		console.log(getAllUser());
-		console.log(`room: ${room}`, getUsersInRoom(room));
+		// console.log(`welcome, ${name}`);
+		// console.log(getAllUser());
 		socket.emit("message", {
 			user: "admin",
-			text: `${user.name}, welcome to the room ${user.room}`
+			text: `${user.name}, welcome to the room ${user.room}`,
 		});
 
 		socket.broadcast
-			.to(user.room)
+			.in(user.room)
 			.emit("message", { user: "admin", text: `${user.name} has joined!` });
 
+		console.log(`room: '${room}'`, getUsersInRoom(room));
 		socket.join(user.room);
+
+		io.in(user.room).emit("roomData", {
+			room: user.room,
+			users: getUsersInRoom(user.room),
+		});
 	});
 
 	socket.on("sendMessage", (message, callback) => {
@@ -58,7 +66,7 @@ io.on("connection", (socket) => {
 			console.log(finding.name, finding.room);
 			io.to(finding.room).emit("message", {
 				user: finding.name,
-				text: message
+				text: message,
 			});
 		}
 		callback();
@@ -68,9 +76,14 @@ io.on("connection", (socket) => {
 		const user = removeUser(socket.id);
 
 		if (user) {
+			console.log(`user '${user.name} had left`);
 			io.to(user.room).emit("message", {
 				user: "admin",
-				text: `${user} has left.`
+				text: `${user.name} has left.`,
+			});
+			io.to(user.room).emit("roomData", {
+				room: user.room,
+				users: getUsersInRoom(user.room),
 			});
 		}
 	});
